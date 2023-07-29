558d67c692b4b2adf5e19cba7b48546b3232ec27
Sam Mahonri <sammahonri06@gmail.com>
2023-07-29 13:00:22 -0300

    .

--- start_modules.js
+++ start_modules.js
@@ -12,7 +12,8 @@ async function carregarTodosOsElementos() {
     try {
         await Promise.all([
             carregarConteudo("nav_area", "./source/modules/navbar.html"),
-            carregarConteudo("footer", "./source/modules/footer.html")
+            carregarConteudo("footer", "./source/modules/footer.html"),
+            carregarConteudo("warn_area", "./source/modules/warning.html")
         ]);
         console.log("[ OwO ] Página completamente carregada com êxito!");
         if (currentPage == "arts"){
